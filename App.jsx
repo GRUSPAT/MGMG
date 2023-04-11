e24d758f7dc75faf87ddc29860df0374d5e4a8b8
Michal Gronski <michal.gronski@icloud.com>
2023-04-11 14:09:34 +0200

    Splash Screen for Android

--- App.jsx
+++ App.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Lottie from 'lottie-react-native';
+import SplashScreen from 'react-native-lottie-splash-screen'
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
-  <Lottie source={require('./error.json')} autoPlay loop />
+  <Lottie source={require('./assets/animations/error.json')} autoPlay loop />
   );
 };
 
